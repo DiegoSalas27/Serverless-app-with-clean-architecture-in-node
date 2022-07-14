acaa253bc274ce07f2593a2d919955bfa2789af0
dominicsc2 <dominicsc2hs@gmail.com>
2022-07-14 12:03:46 -0400

    chore: add main layer dependencies and configuration

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,8 @@ module.exports = {
     '@data/(.*)$': '<rootDir>/src/data/$1',
     '@infrastructure/(.*)$': '<rootDir>/src/infrastructure/$1',
     '@domain/(.*)$': '<rootDir>/src/domain/$1',
-    '@presentation/(.*)$': '<rootDir>/src/presentation/$1'
+    '@presentation/(.*)$': '<rootDir>/src/presentation/$1',
+    '@main/(.*)$': '<rootDir>/src/main/$1'
   },
   preset: '@shelf/jest-mongodb',
   transform: {
