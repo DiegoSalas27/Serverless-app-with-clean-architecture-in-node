8f28165c2fbe5a12963c62025e2595f130479990
dominicsc2 <dominicsc2hs@gmail.com>
2022-07-14 07:34:18 -0400

    feat: ensure DBAddAccount calls AddAccountRepository with correct values

--- src/data/usecases/account/add-account/db-add-account.ts
+++ src/data/usecases/account/add-account/db-add-account.ts
@@ -1,12 +1,15 @@
 import { AddAccountParams, IAddAccount } from '../../../../domain/usecases/account'
+import { IAddAccountRepository } from '../../../protocols/db/account/add-account-repository'
 import { ILoadAccountByEmailRepository } from '../../../protocols/db/account/load-account-by-email-repository'
 
 export class DbAddAccount implements IAddAccount {
-  constructor(private readonly loadAccountByEmailRepository: ILoadAccountByEmailRepository) {}
+  constructor(private readonly loadAccountByEmailRepository: ILoadAccountByEmailRepository,
+    private readonly addAccountRepository: IAddAccountRepository) {}
 
   async add(accountParams: AddAccountParams): Promise<string> {
     const account = await this.loadAccountByEmailRepository.loadByEmail(accountParams.email)
     if (!account) {
+      await this.addAccountRepository.add(accountParams)
       return ''
     }
 
