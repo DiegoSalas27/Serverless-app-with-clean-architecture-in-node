0d1f66033ba2be8bd001f850621ea09e6788034f
dominicsc2 <dominicsc2hs@gmail.com>
2022-07-14 09:29:14 -0400

    feat: add Http and Controller protocols

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,8 @@ module.exports = {
   moduleNameMapper: {
     '@data/(.*)$': '<rootDir>/src/data/$1',
     '@infrastructure/(.*)$': '<rootDir>/src/infrastructure/$1',
-    '@domain/(.*)$': '<rootDir>/src/domain/$1'
+    '@domain/(.*)$': '<rootDir>/src/domain/$1',
+    '@presentation/(.*)$': '<rootDir>/src/presentation/$1'
   },
   preset: '@shelf/jest-mongodb',
   transform: {
