1c9fa219314a5c8533765a4849150792800a2f92
dominicsc2 <dominicsc2hs@gmail.com>
2022-07-13 20:37:50 -0400

    refactor: add namespacing to IAddAccount usecase protocol

--- src/domain/usecases/account/add-account.ts
+++ src/domain/usecases/account/add-account.ts
@@ -3,5 +3,10 @@ import { User } from '../../models'
 export type AddAccountParams = Omit<User, 'id' | 'tasks'>
 
 export interface IAddAccount {
-  add(account: AddAccountParams): Promise<string>
+  add(account: IAddAccount.Params): Promise<IAddAccount.Result>
+}
+
+export namespace IAddAccount {
+  export type Params = AddAccountParams
+  export type Result = string
 }
